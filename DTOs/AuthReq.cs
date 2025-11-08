0f62fd7c6c4a607cc6395412621a54a35b905c4f
binna <every5116@naver.com>
2025-11-09 04:09:29 +0900

    관심사 분리에 따른 DTO 변경, 새로운 에러 코드 추가

--- DTOs/AuthReq.cs
+++ DTOs/AuthReq.cs
@@ -4,9 +4,9 @@ namespace SharedLibrary.Protocol.DTOs;
 
 public class AuthReq
 {
-    public string? AuthType { get; set; }
-    public string? Nickname { get; set; }
-    public string? Email { get; set; }
-    public string? Pw { get; set; }
+    public string AuthType { get; set; }
+    public string Nickname { get; set; }
+    public string Email { get; set; }
+    public string Pw { get; set; }
     public string Language { get; set; } = $"{AppEnum.ELanguage.En}";
 }
