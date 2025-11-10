1471337efcd730ebea46e3dddfb5496c0ee13f17
binna <every5116@naver.com>
2025-11-10 12:41:45 +0900

    AuthReq 수정

--- DTOs/AuthReq.cs
+++ DTOs/AuthReq.cs
@@ -5,7 +5,7 @@ namespace SharedLibrary.Protocol.DTOs;
 public class AuthReq
 {
     public string AuthType { get; set; }
-    public string Nickname { get; set; }
+    public string? Nickname { get; set; }
     public string Email { get; set; }
     public string Pw { get; set; }
     public string Language { get; set; } = $"{AppEnum.ELanguage.En}";
