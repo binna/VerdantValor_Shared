47ba6ef2662a7a9b38ab628b1803bee9cacfdf3f
binna <every5116@naver.com>
2025-11-11 15:47:11 +0900

    - 시간을 바꾸고, 시간을 리셋을 위한 컨트롤러 Req 만듦

--- DTOs/CreateScoreReq.cs
+++ DTOs/CreateScoreReq.cs
@@ -2,6 +2,6 @@
 
 public class CreateScoreReq
 {
-    public string? Type { get; set; }
+    public string Type { get; set; }
     public double Score { get; set; }
 }
--- DTOs/GetRankReq.cs
+++ DTOs/GetRankReq.cs
@@ -2,8 +2,8 @@
 
 public class GetRankReq
 {
-    public string? Scope { get; set; }
-    public string? Type { get; set; }
+    public string Scope { get; set; }
+    public string Type { get; set; }
     public sbyte Limit { get; set; } = 50;
 
 }
