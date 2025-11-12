5d2305405f71710b54a092fdc95bd9ea05b4828b
binna <every5116@naver.com>
2025-11-12 13:00:38 +0900

    API Request Body용 req, res DTO

---
+++ DTOs/EncryptReq.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedLibrary.Protocol.DTOs;
+
+public class EncryptReq
+{
+    public string Nonce { get; set; }
+    public string Tag { get; set; }
+    public string Data { get; set; }
+}
---
+++ DTOs/EncryptRes.cs
@@ -0,0 +1,7 @@
+﻿namespace SharedLibrary.Protocol.DTOs;
+
+public class EncryptRes
+{
+    public string Tag { get; set; }
+    public byte[] Data { get; set; }
+}
