d0549863000de5445b032c0cd37d659724ef49be
binna <every5116@naver.com>
2025-12-28 21:40:57 +0900

    [#25] 기존 작업 리팩터링
    - 에러 메시지 구조 개선
      상태 코드 enum을 서브모듈로 분리하여 클라이언트와 공용 사용

--- Common/Web/AppEnum.cs
+++ Common/Web/AppEnum.cs
@@ -24,4 +24,34 @@ public static class AppEnum
         Ko = 0,
         En = 1,
     }
+    
+    public enum EResponseStatus
+    {
+        // 1000 요청 성공
+        Success = 1000,
+        SuccessEmptyRanking = 1001,
+    
+        // 그 외의 에러
+        EmptyRequiredField = 2000,
+        InvalidInput =  2001,
+        NoData = 2002,
+        FailDecrypt = 2003,
+    
+        EmailAlphabetNumberOnly = 2010,
+        EmailAlreadyExists = 2011,
+        NicknameAlreadyExists = 2012,
+        InvalidEmailLength = 2013,
+        InvalidNicknameLength = 2014,
+        ForbiddenEmail = 2015,
+        ForbiddenNickname = 2016,
+        NotMatchPw  = 2017,
+        NicknameAlphabetKoreanNumberOnly = 2018,
+    
+        ScoreCannotBeNegative = 2020,
+    
+        // 시스템 에러
+        RedisError = 9997,
+        DbError = 9998,
+        UnexpectedError = 9999,
+    }
 }
---
+++ GameData/ResponseStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedLibrary.Protocol.GameData;
+
+public class ResponseStatus
+{
+    public List<ResponseStatusItem> Data { get; set; } = new();
+}
+
+public class ResponseStatusItem
+{
+    public int Code { get; set; }
+    public bool IsSuccess { get; set; }
+    public List<string> Message { get; set; }
+}
