e3c7fc14d8bca5fded9a36a0cc3abca900ddb38d
binna <every5116@naver.com>
2025-12-30 00:47:53 +0900

    [#27] 채팅 서버 기능 만들기
    - 현재 들어가있는 방을 나가면 되기 때문에 굳이 방 id 정보가 필요 없어서 삭제

--- Packet/ChatServer/ExitRoomPayload.cs
+++ Packet/ChatServer/ExitRoomPayload.cs
@@ -1,24 +1,14 @@
-﻿using System.Buffers.Binary;
-
-namespace SharedLibrary.Protocol.Packet.ChatServer;
+﻿namespace SharedLibrary.Protocol.Packet.ChatServer;
 
 public class ExitRoomPayload : IPayload
 {
-    public short PayloadSize { get; } = sizeof(int);
-    
-    public int RoomId { get; set; }
+    public short PayloadSize { get; } = 0;
     
     public void Parse(byte[] buffer)
-    {
-        RoomId = BinaryPrimitives.ReadInt32BigEndian(buffer);
-    }
+    { }
 
     public byte[] From()
     {
-        var buffer = new byte[PayloadSize];
-
-        BinaryPrimitives.WriteInt64BigEndian(buffer, RoomId);
-
-        return buffer;
+        return Array.Empty<byte>();
     }
 }
