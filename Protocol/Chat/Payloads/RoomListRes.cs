032823295ced3aa276666c1651e69835ec61bd6f
binna <every5116@naver.com>
2026-02-06 20:33:13 +0900

    [#34] 채팅 서버 요청 처리 결과(Response) 구조 추가
    - 커밋 누락된 RoomListRes 추가

---
+++ Protocol/Chat/Payloads/RoomListRes.cs
@@ -0,0 +1,12 @@
+﻿using MemoryPack;
+using Protocol.Chat.Frames;
+
+namespace Protocol.Chat.Payloads;
+
+[MemoryPackable]
+public partial struct RoomListRes : IPacketBody
+{
+    public int Code { get; set; }
+    public int RoomCount { get; set; }
+    public int[] RoomIds { get; set; }
+}
